3895ded28beb8566178470f027a09b6df09598ed
Dan Dumitrescu <ddumitrescu@wisc.edu>
2025-03-01 15:03:37 -0600

    added several lines prompting player for initial data for the game, tried initializing but that won't work until backend is working properly

--- CODE/Frontend.java
+++ CODE/Frontend.java
@@ -1,5 +1,8 @@
 // package project; // fix this with whole project
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 /*
@@ -19,13 +22,36 @@ public class Frontend {
         }
     }
 
-    public static void displayTextFunnily(String text)
+    public static void displayTextSlowly(String text)
     {
         for(char c : text.toCharArray())
         {
             System.out.print(c);
-            wait(500);
+            wait(30);
         }
+    }
+
+    public static void displayTextSlowly(String text, int milliseconds)
+    {
+        displayTextSlowly(text);
+        wait(milliseconds);
+    }
+    
+    public static int parseInt(Scanner scanner, String property)
+    {
+        int num = 0;
+
+        boolean validInput = false;
+        while (!validInput) {
+            try {
+                num = Integer.parseInt(scanner.nextLine());
+                validInput = true; // If we get here, input was valid
+            } catch (NumberFormatException e) {
+                displayTextSlowly("Didn't quite get that, whats your " + property + "?\n");
+            }
+        }
+
+        return num;
     }
 
     public static void introSlide(Scanner scanner) {
@@ -41,21 +67,69 @@ public class Frontend {
         System.out.println("*                                                  *");
         System.out.println("*        Will you make it to Cerberus XVII?        *");
         System.out.println("****************************************************");
-        
+        /////// Intro block
+        /// 
+        /// 
         /* Now we do the setup */
         System.out.print("USERNAME: ");
         String username = scanner.nextLine();        
-        System.out.println("Welcome, Captain " + username + "! \n");
+        displayTextSlowly("Welcome, Captain " + username + "! \n\n");
 
-
-        System.out.print("What is the name of your ship: ");
-        String shipName = scanner.nextLine();  ;
-
-    
-        System.out.println("How large is your crew")
-
-        /////// Intro block
+        //get the ship name
+        displayTextSlowly("What will you call your ship: ");
+        String shipName = scanner.nextLine(); 
+        displayTextSlowly("Excellent Name!!!\n\n", 1000);
         
+        //get the number of crew
+        displayTextSlowly("How large is your crew: ");
+        int crewNum = -1; 
+
+        while (crewNum < 1 || crewNum > 15)
+        {
+            crewNum = parseInt(scanner, "crew size");
+
+            if(crewNum < 1) 
+            {
+                displayTextSlowly("Really ... ", 1000);
+                displayTextSlowly("you need SOMEONE to manage the crew!!! \n", 1000);
+            }
+            if(crewNum > 10)
+            {
+                displayTextSlowly("Hmm ... ", 1000);     
+                displayTextSlowly("That\'s a bit too many mouths to feed. \n", 1000);   
+            }       
+        }
+
+        displayTextSlowly("Excellent!!!\n\n", 1000);
+
+        //initial morale
+        displayTextSlowly("On a scale of 1 to 100, how do you feel about this journey!\n");
+        int initialMorale = -1; 
+
+        while (initialMorale < 1 || initialMorale > 100)
+        {
+            initialMorale = parseInt(scanner, "morale");
+
+            if(initialMorale < 30) displayTextSlowly("Oh come on you've got to have more than that! ... \n", 1000);
+            if(initialMorale > 70) displayTextSlowly("Woahhh OK, let's dial it down a little! ... \n", 1000);            
+        }
+
+        displayTextSlowly("Excellent!!!\n\n", 1000);
+       
+        //resources
+        displayTextSlowly("How many resources are you planning to fill your ship with ... \n", 1000);
+        displayTextSlowly("Oh ...\n", 1000);
+        displayTextSlowly("And just so you know ...\n", 1000);
+        displayTextSlowly("more resources means less cargo space ... \n", 1000);
+        int initalResourceCount = parseInt(scanner, "initial resouce count");
+
+        //set up the player count
+        //List<Event> events = new ArrayList<Event>(); //theoretically there should be a way to fill this with events
+        //Player player = new Player(0, events, crewNum, initialMorale, initalResourceCount, shipName);
+
+        wait(2000);
+        displayTextSlowly("Upload Complete\n");
+
         // THIS IS NOT FINISHED, JUST SIMULATED FOR TESTING///////////////////////////////////////
         boolean connected = false;
         int attempts = 0;
