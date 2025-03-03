9f65869e578d58c2cc74ca7ac2fb9351f93175c0
Will Geister <wgeister@wisc.edu>
2025-03-03 15:25:19 -0600

    added SQL DB files from VM (Copied over, work was done previously to 3/3)

--- CODE/run_game.bat
+++ CODE/run_game.bat
@@ -13,8 +13,15 @@ if not errorlevel 1 (
     docker rm %CONTAINER_NAME%
 )
 
+echo Ensuring network exists...
+docker network ls | findstr /C:"game_network" >nul
+if errorlevel 1 docker network create game_network
+
+echo Connecting PostgreSQL to the network...
+docker network connect game_network planet_db
+
 echo Starting container...
-start /b docker run -d --name %CONTAINER_NAME% %IMAGE_NAME%
+start /b docker run -d --name %CONTAINER_NAME% --network=game_network %IMAGE_NAME%
 
 echo Waiting for container to start...
 timeout /t 5 >nul
